265862f91a774b2ae07d5000749a2f2313d2b2cd
mukul <mukulghanghas1@gmail.com>
2022-07-01 18:49:10 +0530

    Minor changes

---
+++ app/src/main/java/com/example/chatapp/MessageDataClass.kt
@@ -0,0 +1,12 @@
+package com.example.chatapp
+
+
+data class MessageDataClass (
+    var text: String? = null,
+    var name: String? = null,
+    var photoUrl: String? = null,
+    var imageUrl: String? = null
+)
+
+
+
